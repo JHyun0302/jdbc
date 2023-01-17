20dcd8554cb9895122f38990fbb251011f4ef0ef
jaehyun <wogustkfkd11@gmail.com>
2023-01-17 23:56:41 +0900

    직접 만든 중복 키 에러, Db에러로 예외 잡기

---
+++ src/test/java/hello/jdbc/exception/translator/ExTranslatorV1Test.java
@@ -0,0 +1,99 @@
+package hello.jdbc.exception.translator;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.ex.MyDbException;
+import hello.jdbc.repository.ex.MyDuplicateKeyException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Random;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+public class ExTranslatorV1Test {
+    Repository repository;
+    Service service;
+
+    @BeforeEach
+    void init() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        repository = new Repository(dataSource);
+        service = new Service(repository);
+    }
+
+    @Test
+    void duplicateKeySave() {
+        service.create("myId");
+        service.create("myId");
+    }
+
+    @Slf4j
+    @RequiredArgsConstructor
+    static class Service {
+        private final Repository repository;
+
+        /**
+         * 내가 만든 MyDuplicateKeyException, MyDbException으로 예외 잡음
+         * Jdbc, Jpa 등 기술들에 의존은 안하지만 키 중복, 무슨 에러 등 에러마다 내가 예외를 하나하나 만들기는 어려움!
+         */
+        public void create(String memberId) {
+            try {
+                repository.save(new Member(memberId, 0));
+                log.info("saveId={}", memberId);
+            } catch (MyDuplicateKeyException e) {
+                log.info("키 중복, 복구 시도");
+                String retryId = generateNewId(memberId);
+                log.info("retryId={}", retryId);
+                repository.save(new Member(retryId, 0));
+            } catch (MyDbException e) {
+                log.info("데이터 접근 계층 예외", e);
+                throw e;
+            }
+
+        }
+
+        private String generateNewId(String memberId) {
+            return memberId + new Random().nextInt(10000);
+        }
+    }
+
+    @RequiredArgsConstructor
+    static class Repository {
+        private final DataSource dataSource;
+
+        public Member save(Member member) {
+            String sql = "insert into member (member_id, money) values(?,?)";
+
+            Connection con = null;
+            PreparedStatement pstmt = null;
+
+            try {
+                con = dataSource.getConnection();
+                pstmt = con.prepareStatement(sql);
+                pstmt.setString(1, member.getMemberId());
+                pstmt.setInt(2, member.getMoney());
+                pstmt.executeUpdate();
+                return member;
+            } catch (SQLException e) {
+                //h2 db
+                if (e.getErrorCode() == 23505) {
+                    throw new MyDuplicateKeyException(e);
+                }
+                throw new MyDbException(e);
+            } finally {
+                JdbcUtils.closeStatement(pstmt);
+                JdbcUtils.closeConnection(con);
+            }
+
+
+        }
+    }
+}
