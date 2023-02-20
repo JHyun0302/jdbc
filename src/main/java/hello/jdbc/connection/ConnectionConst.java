ac1dd7ecde2af3bb609921c8ff735074fb359e03
jaehyun <wogustkfkd11@gmail.com>
2023-02-21 00:03:43 +0900

    설명 추가

--- src/main/java/hello/jdbc/connection/ConnectionConst.java
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -1,11 +1,11 @@
 package hello.jdbc.connection;
 
+/**
+ * DB에 접속하는데 필요한 기본 정보를 상수로 만듬!
+ * 따로 객체 생성하면 안되므로 abstract class 선언
+ */
 public abstract class ConnectionConst {
     public static final String URL = "jdbc:h2:tcp://localhost/~/test";
     public static final String USERNAME = "sa";
     public static final String PASSWORD = "";
 }
-/**
- * DB에 접속하는데 필요한 기본 정보를 상수로 만듬!
- * 따로 객체 생성하면 안되므로 abstract class 선언
- */
